f1d37f4dfd5b2e266f741ee35c2cb6f25ee6031c
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-20 17:09:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 def prime?(number)
   if number <= 1 
     return false 
-  elsif number == 3 || number == 2 
-    return true
+  #elsif number == 3 || number == 2 
+   # return true
   elsif number.even?
     return false 
   else 
-    (4..(number - 1)).each do |num|
+    (2..(number - 1)).each do |num|
       return false if number % num == 0 
       end 
   end 
