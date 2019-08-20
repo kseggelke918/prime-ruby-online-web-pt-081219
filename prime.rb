7a2a75dc260f8df1b3e3fa11121239f26fad460b
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-20 17:02:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,8 +6,8 @@ def prime?(number)
   elsif number.even?
     false 
   else 
-    (4..(number - 1)).none? do |num|
-      number % num == 0 
+    (4..(number - 1)).each do |num|
+      return false if number % num == 0 
     end 
     #number % anything_but_number_or_1 != 0 
   end 
