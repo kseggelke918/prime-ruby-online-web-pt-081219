4680aabd5b576ddd78f2ed7178e5892c6350f69c
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-20 16:45:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(argument)
+  if argument <= 1 
+    false 
+  elsif argument == 3 || argument == 2 
+    true
+  elsif argument.even?
+    false 
+  else 
+    #argument % anything_but_argument_or_1 != 0 
+    
+  end 
+    
+    #not a prime number
+  #number must be only divisble by 1 and itself 
+    #if n/n2 doesn't have a remainder - not prime 
+  
+end 
+
